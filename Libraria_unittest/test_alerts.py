16ed7fc319e94a66d170e510ee0a37e8e8d39f3b
Gabriela R <98127380+Aktana@users.noreply.github.com>
2023-06-07 12:05:17 +0300

    unit test

--- Libraria_unittest/test_alerts.py
+++ Libraria_unittest/test_alerts.py
@@ -112,41 +112,5 @@ Rularea testelor:
 2. cd <cale_fisier> + pytest nume_fisier.py
 
 pentru instalare pytest: din terminal rulati pip install pytest
-"""
 
-"""
-testare pozitiva = testarea situatiilor favorabile pe care sistemul ar trebui sa le poata executa si accepta
-testare negativa = testarea situatiilor nefavorabile pe care sistemul ar trebui sa nu le poata executa dar sa le trateze corect
-
-testare functionala = tip de testare care se face pentru a verifica daca sistemul isi indeplineste functiile
-testare non-functionala = tip de testare care se face pentru a verifica cat de bine isi indeplineste sistemul functiile
-Exemple de teste non-functionale:
-a) Performance testing = tip de testare care verifica viteza de reactie a sistemului
-- stress testing = tip de testare non-functionala din categoria performance testing care verifica felul in care se comporta sistemul atunci cand este aproape sa isi atinga limita superioară 
-- load testing = tip de testare non-functionala din categoria performance testing care verifica felul in care se comporta sistemul la un anumit nivel de incarcare de utilizatori
-- volume testing = tip de testare non-functionala din categoria performance testing care verifica felul in care se comporta sistemul la un anumit nivel de incarcare de date
-
-- spike testing = tip de testare non-functionala din categoria performance testing care verifica felul in care se comporta sistemul atunci cand are o crestere brusca de utilizatori sau de date 
-- scalability testing = tip de testare non-functionala din categoria performance testing care verifica felul in care se comporta sistemul atunci cand are o crestere treptata in numarul de utilizatori sau de date
-- reliability testing = tip de testare non-functionala din categoria performance testing care verifica felul in care se comporta sistemul la un anumit nivel de incarcare pe o anumita perioada de timp
-
-b) Recovery testing = Tip de testare non-functionala care verifica felul in care sistemul isi revine in urma unui crash (dupa ce i s-a depasit limita superioara)
-
-c) Usability testing = Tip de testare non-functionala care verifica cat de usor ii este utilizatorului sa foloseasca sistemul software (evalueaza experienta utilizatorului. Ex: daca arata bine, daca este intuitiv, daca este usor de folosit, daca optiunile sunt vizibile etc)
-- 5 seconds test este un tip de usability testing care verifica cantitatea de informatii/parerea utlizatorului pe care poate sa o obtina in primele 5 secunde de interactiune cu produsul software
-- A&B este un tip de usability testing prin care se plaseaza utilizatorului doua versiuni ale aceluiasi produs pentru a vedea care este cel cu cele mai favorabile review-uri
-
-d) Portability testing = tip de testare non-functionala care verifica felul in care sistemul se comporta atunci cand este migrat de pe o platforma pe cealalta (ex: migrare intre sisteme de operare, migrare intre limbaje de programare, migrare intre locatia produsul - ex server etc)
-
-e) Maintainability testing = tip de testare non-functionala care verifica faptul ca produsul este usor de mentinut
-
-mentenanta (maintenance) = proces prin care produsul este modificat fie pentru a aduce functionalitati noi fie pentru a modifica/corecta functionalitatile existente
-
-f) Security testing = tip de testare non-functionala care verifica daca produsul este suficient de protejat impotriva atacurilor cibernetice
-
-g) Compatibility testing = tip de testare non-functionala care verifica fie compatibilitatea intre sisteme, fie compatibilitatea sistemului existent cu un anumit set de date
-
-h) Localization testing = tip de testare non-functionala care verifica adaptabilitatea sistemului la diverse particularitati culturale (ex: diacritice, caractere japoneze/chinezesti etc)
-
-i) Compliance regulation testing = tip de testare non-functionala care verifica daca sistemul este compliant cu diverse situatii contractuale sau legale (ex: GDPR)
 """
